4d9ce53e0a449a4fd91f1974e425fe3f545b6178
Eduardo <edonoso2@gmail.com>
2024-04-06 14:42:39 -0300

    Teste da lib requests

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from fastapi.responses import FileResponse
 
 from pydantic import BaseModel
 
+from datetime import datetime
+import requests
+
+
 app = FastAPI()
 
 
@@ -10,9 +14,34 @@ class Item(BaseModel):
     item_id: int
 
 
+def now():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def request_url(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("GET error", response.status_code, "- URL:", url)
+    
+    return response
+
+
+def get_event_dates(month=4, year=2024):
+    event_code = "mcXTJpXoZh8bbnvjtRmi"
+    
+    url = "https://services.tix.byinti.com/neofront-v3/ticket-event-dates/?"
+    url += "event_code={0}&month={1}&year={2}".format(event_code, month, year)
+        
+    return request_url(url)
+    
+    
 @app.get("/")
 async def root():
-    return {"message": "Message"}
+    response = get_event_dates()
+        
+    return {"message": response.text}
 
 
 @app.get('/favicon.ico', include_in_schema=False)
